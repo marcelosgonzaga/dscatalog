1a07ca5e5cece46878cf886e70eb89764d0c389c
Marcelo Gonzaga <marcelosgonzaga@gmail.com>
2024-06-23 20:46:31 -0300

    update

--- backend/src/main/java/com/dscatalog/backend/services/CategoryService.java
+++ backend/src/main/java/com/dscatalog/backend/services/CategoryService.java
@@ -1,6 +1,5 @@
 package com.dscatalog.backend.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -12,6 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dscatalog.backend.dto.CategoryDTO;
 import com.dscatalog.backend.entities.Category;
 import com.dscatalog.backend.repositories.CategoryRepository;
+import com.dscatalog.backend.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -46,6 +48,21 @@ public class CategoryService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+		
+	}
+	
+	@Transactional
+	public CategoryDTO update(Long id,  CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+	}
+	
 	}
 
 }
