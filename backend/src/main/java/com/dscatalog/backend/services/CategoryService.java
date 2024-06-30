28bdcfb55cf8a5ac56c81a35bd9694346bd788af
Marcelo Gonzaga <marcelosgonzaga@gmail.com>
2024-06-30 13:49:29 -0300

    Pagination

--- backend/src/main/java/com/dscatalog/backend/services/CategoryService.java
+++ backend/src/main/java/com/dscatalog/backend/services/CategoryService.java
@@ -1,19 +1,22 @@
 package com.dscatalog.backend.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dscatalog.backend.dto.CategoryDTO;
 import com.dscatalog.backend.entities.Category;
 import com.dscatalog.backend.repositories.CategoryRepository;
+import com.dscatalog.backend.services.exceptions.DatabaseException;
 import com.dscatalog.backend.services.exceptions.ResourceNotFoundException;
 
-import jakarta.persistence.EntityNotFoundException;
+//import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -22,22 +25,15 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
-		List<Category> list =  repository.findAll();
-		
-//		List<CategoryDTO> listDto = new ArrayList<>();
-//		for(Category cat : list) {
-//			listDto.add(new CategoryDTO(cat));
-//		}
-		
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-	
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Category> list =  repository.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x));
 	}
 
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.get();
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException ("Entity not found"));
 		return new CategoryDTO(entity);
 
 	}
@@ -59,10 +55,23 @@ public class CategoryService {
 			entity = repository.save(entity);
 			return new CategoryDTO(entity);
 		}
-		catch(EntityNotFoundException e) {
+		catch(ResourceNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found" + id);
-	}
+		}
 	
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e){
+			throw new DatabaseException ("Integrety Violatin");
+		}
+		
 	}
 
 }
--- backend/src/main/java/com/dscatalog/backend/services/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/dscatalog/backend/services/exceptions/ResourceNotFoundException.java
@@ -2,8 +2,9 @@ package com.dscatalog.backend.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
-
+	
 	public ResourceNotFoundException(String msg) {
 		super(msg);
 	}
+
 }
